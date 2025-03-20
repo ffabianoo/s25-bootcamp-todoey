82982bc8d37d7f9a186b2591d17d39bac016738e
Francesca <ffabiano@unc.edu>
2025-03-20 10:59:03 -0400

    Todoey completed

---
+++ ToDoey/ToDoeyTests/ToDoeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoeyTests.swift
+//  ToDoeyTests
+//
+//  Created by Francesca Fabiano-Grossi on 3/18/25.
+//
+
+import Testing
+@testable import ToDoey
+
+struct ToDoeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
